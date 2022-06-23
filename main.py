d5d75dd5f821893844a7573c99ef2ce93c16cef0
Jaecobd1 <jaecob.d1@gmail.com>
2022-06-22 20:37:21 -0400

    added map

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ def attack(character):
     print("attacking...")
 
 
-def multiply_by2(li):
-    new_list = []
-    for item in li:
-        new_list.append(item*2)
-    return new_list
+def multiply_by2(item):
+    return (item * 2)
 
 
-print(multiply_by2([1, 2, 3]))
+# map, filter, zip and reduce
+print(list(map(multiply_by2, [1, 2, 3])))
